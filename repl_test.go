77e0f6c1d423ded065e8f7cc100bb26d1243334b
Aaron Fleming <amfleming15@gmail.com>
2026-01-14 18:59:08 -0800

    Add input cleaning

---
+++ repl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestCleanInpt(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("FAIL: Received %d words, Expected %d", len(actual), len(c.expected))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("FAIL: '%s' != '%s' (expected)", word, expectedWord)
+			}
+		}
+	}
+}
