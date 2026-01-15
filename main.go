a2daa6bcddc78884dcc58e1317d45f1dcd02643b
Aaron Fleming <amfleming15@gmail.com>
2026-01-15 00:32:11 -0800

    Add 'map' and 'mapb' commands; refactor command registry

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
 
+var commands map[string]cliCommand
+
+type Config struct {
+	Next     string
+	Previous string
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
 }
 
-var commands map[string]cliCommand
+type locationAreaQuery struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
 
 func init() {
 	commands = map[string]cliCommand{
@@ -27,6 +44,16 @@ func init() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 map locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 map locations",
+			callback:    commandMapb,
+		},
 	}
 }
 
@@ -34,13 +61,13 @@ func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
-func commandExit() error {
+func commandExit(config *Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *Config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Print("Usage:\n\n")
 
@@ -50,8 +77,42 @@ func commandHelp() error {
 	return nil
 }
 
+func commandMap(config *Config) error {
+	res, err := http.Get(config.Next)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var jsonData locationAreaQuery
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&jsonData)
+	if err != nil {
+		return err
+	}
+	for _, location := range jsonData.Results {
+		fmt.Println(location.Name)
+	}
+	config.Next = jsonData.Next
+	config.Previous = jsonData.Previous
+	return nil
+}
+
+func commandMapb(config *Config) error {
+	if config.Previous == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	config.Next = config.Previous
+	return commandMap(config)
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	config := Config{
+		Next:     "https://pokeapi.co/api/v2/location-area/?limit=20&offset=0",
+		Previous: "",
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -67,7 +128,7 @@ func main() {
 			fmt.Println("Unknown command")
 			continue
 		}
-		err := command.callback()
+		err := command.callback(&config)
 		if err != nil {
 			fmt.Println(err)
 		}
