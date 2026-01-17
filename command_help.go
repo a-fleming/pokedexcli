63e0e04aa14c01ac8f38c3c5c93c4cb010a6e7dc
Aaron Fleming <amfleming15@gmail.com>
2026-01-17 05:38:04 -0800

    Sort output of 'help' command

--- command_help.go
+++ command_help.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+	"slices"
+)
 
 func commandHelp(cfg *config, args ...string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
-	for _, command := range getCommands() {
+	commands := getCommands()
+	for _, commandName := range slices.Sorted(maps.Keys(commands)) {
+		command := commands[commandName]
 		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 	fmt.Println()
